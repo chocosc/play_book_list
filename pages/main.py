7c935cb6e08a73cfaa93228d4fedce81b37e4de9
hhb0 <131655783+hhb0@users.noreply.github.com>
2023-10-03 22:54:05 +0900

    Update main.py

--- pages/main.py
+++ pages/main.py
@@ -38,7 +38,7 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
-cred = credentials.Certificate
+cred = credentials.Certificate("./.streamlit/playbooklist.json")
 firebase_admin.initialize_app(cred)
 
 @st.cache_resource(show_spinner=None)
